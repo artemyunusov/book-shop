705e3bcb44967c6a1ffa5a9aa120359c9174bc06
artemyunusov <artemyunusov@gmail.com>
2021-04-07 17:17:57 +0500

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,9 +27,7 @@ $('.slider').slick({
         slidesToScroll: 1
       }
     }
-    // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
-    // instead of a settings object
   ]
 });
 
